1f7d58f29b92172b1ccb90d0180d54aecd56a780
Lee Jiwoon <webmaster@kw.ac.kr>
2019-08-05 23:32:29 +0900

    Core and Videos Add

---
+++ videotoimage_itzy.py
@@ -0,0 +1,20 @@
+import cv2
+vidcap = cv2.VideoCapture('video_ITZY.mp4')
+
+def getFrame(sec):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+        cv2.imwrite("itzy/itzy_image"+str(count)+".png", image)     # save frame as PNG file
+    return hasFrames
+
+
+sec = 0
+frameRate = 1/30 # it will capture image in each 0.5 second
+count = 1
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec, 2)
+    success = getFrame(sec)
