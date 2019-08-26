cf4c67f45368ccfe5579d36dbc138ba8c48a27ea
Lee Jiwoon <webmaster@kw.ac.kr>
2019-08-26 15:56:36 +0900

    csv concaternation

--- videotoimage_bts.py
+++ videotoimage_bts.py
@@ -1,20 +1,14 @@
 import cv2
-vidcap = cv2.VideoCapture('video_BTS.mp4')
 
-def getFrame(sec):
-    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
-    hasFrames,image = vidcap.read()
-    if hasFrames:
-        cv2.imwrite("bts/bts_image"+str(count)+".png", image)     # save frame as PNG file
-    return hasFrames
+# Opens the Video file
+cap = cv2.VideoCapture('video_BTS.mp4')
+i = 0
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    cv2.imwrite("BTS/bts_image"+str(i)+".png", frame)
+    i += 1
 
-
-sec = 0
-frameRate = 1/24
-count = 1
-success = getFrame(sec)
-while success:
-    count = count + 1
-    sec = sec + frameRate
-    sec = round(sec, 2)
-    success = getFrame(sec)
+cap.release()
+cv2.destroyAllWindows()
--- videotoimage_itzy.py
+++ videotoimage_itzy.py
@@ -1,20 +1,14 @@
 import cv2
-vidcap = cv2.VideoCapture('video_ITZY.mp4')
 
-def getFrame(sec):
-    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
-    hasFrames,image = vidcap.read()
-    if hasFrames:
-        cv2.imwrite("itzy/itzy_image"+str(count)+".png", image)     # save frame as PNG file
-    return hasFrames
+# Opens the Video file
+cap = cv2.VideoCapture('video_ITZY.mp4')
+i = 0
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    cv2.imwrite("ITZY/itzy_image"+str(i)+".png", frame)
+    i += 1
 
-
-sec = 0
-frameRate = 1/24
-count = 1
-success = getFrame(sec)
-while success:
-    count = count + 1
-    sec = sec + frameRate
-    sec = round(sec, 2)
-    success = getFrame(sec)
+cap.release()
+cv2.destroyAllWindows()
